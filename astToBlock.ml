6ecbc37bd80638d2ff9b56807f47f6a014818895
Haruka Matsumoto <harukam0416@gmail.com>
2018-12-16 15:22:22 +0900

    Initial commit
    - This project is for printing the AST of OCaml program as XML.
    - my_compile.ml, pprintast2.ml, and main.ml are copied from
      Stepper project. (Reference: http://pllab.is.ocha.ac.jp/~asai/)
    - Add new file named astToBlock.ml, but it does nothing yet.
    - xml.ml provides datatype and functions which are useful to
      handle XML element as an OCaml data structure. They are copied
      from my other projects.

---
+++ astToBlock.ml
@@ -0,0 +1,151 @@
+open Parsetree
+open Asttypes
+open Longident
+
+exception NotImplemented of string
+
+let pp = Format.fprintf
+(* https://caml.inria.fr/pub/docs/manual-ocaml/libref/Parsetree.html#TYPEexpression *)
+(* https://caml.inria.fr/pub/docs/manual-ocaml/libref/Asttypes.html#TYPErec_flag *)
+
+(* miscellaneous printers *)
+(* TODO move these *)
+let dom_rec_flag = function
+  |	Nonrecursive -> raise (NotImplemented "Nonrecursive")
+  |	Recursive -> raise (NotImplemented "Recursive")
+
+let dom_direction_flag = function
+  | Upto -> raise (NotImplemented "Upto")
+  | Downto -> raise (NotImplemented "Downto")
+
+let dom_mutable_flag = function
+  | Immutable -> raise (NotImplemented "Immutable")
+  | Mutable -> raise (NotImplemented "Mutable")
+
+let dom_private_flag = function
+  | Private -> raise (NotImplemented "Private")
+  | Public -> raise (NotImplemented "Public")
+
+let dom_virtual_flag = function
+  | Virtual -> raise (NotImplemented "Virtual")
+  | Concrete -> raise (NotImplemented "Concrete")
+
+let dom_override_flag = function
+  | Override -> raise (NotImplemented "Override")
+  | Fresh -> raise (NotImplemented "Fresh")
+
+let dom_strings () = raise (NotImplemented "dom_strings")
+
+(* The type of identifiers (including path like Foo(X).Bar.y) *)
+let rec dom_ident = function
+  | Lident (name) -> raise (NotImplemented "Lident")
+    (* i . i *) (** Access in module *)
+  | Ldot (ident, name) -> raise (NotImplemented "LDot")
+    (* i i *) (** Application *)
+  | Lapply (ident1, ident2) -> raise (NotImplemented "Lapply")
+
+(* The type of patterns *)
+and dom_patt patt = match patt with
+  |	Ppat_any -> raise (NotImplemented "Any")
+  |	Ppat_var (loc) -> raise (NotImplemented ("tvar" ^ loc.txt))
+  |	Ppat_tuple (patt_list) -> raise (NotImplemented "tuplepatt")
+  |	Ppat_record _ -> raise (NotImplemented "patt-record")
+  |	Ppat_type _ -> raise (NotImplemented "patt-type")
+  | _ -> raise (NotImplemented "patt")
+
+(* The type of expressions *)
+and dom_expr expr = match expr.pexp_desc with
+  |	Pexp_ident loc -> dom_ident loc.txt
+  |	Pexp_let (rec_flag, binding, expr) -> raise (NotImplemented "let")
+  |	Pexp_fun _ -> raise (NotImplemented "fun")
+  |	Pexp_apply _ -> raise (NotImplemented "apply")
+  |	Pexp_match _ -> raise (NotImplemented "match")
+  |	Pexp_try _ -> raise (NotImplemented "try")
+  |	Pexp_tuple _ -> raise (NotImplemented "tuple")
+  |	Pexp_variant _ -> raise (NotImplemented "variant")
+  |	Pexp_record _ -> raise (NotImplemented "Pexp_record")
+  |	Pexp_ifthenelse _ -> raise (NotImplemented "ifthenelse")
+  | _ -> raise (NotImplemented "expr")
+
+(* The type of structure items *)
+and dom_struct_item item = match item.pstr_desc with
+  |	Pstr_eval (expression, _) -> dom_expr expression
+  |	Pstr_value (rec_flag, bindings) -> raise (NotImplemented "Pstr_value")
+  |	Pstr_primitive (value_description) -> raise (NotImplemented "Pstr_primitive")
+  |	Pstr_type (rec_flag, type_declarations) -> raise (NotImplemented "Pstr_type")
+  |	Pstr_typext (type_extension) -> raise (NotImplemented "Pstr_typext")
+  |	Pstr_exception (extension_constructor) -> raise (NotImplemented "exception")
+  |	Pstr_module (module_binding) -> raise (NotImplemented "Pstr_module")
+  |	Pstr_recmodule (module_bindings) -> raise (NotImplemented "Pstr_recmodule")
+  |	Pstr_modtype (module_type_declaration) -> raise (NotImplemented "Pstr_modtype")
+  |	Pstr_open (open_description) -> raise (NotImplemented "Pstr_modtype")
+  |	Pstr_class (class_declarations) -> raise (NotImplemented "Pstr_class")
+  |	Pstr_class_type (class_type_declarations) -> raise (NotImplemented "Pstr_class_type")
+  |	Pstr_include (include_declaration) -> raise (NotImplemented "Pstr_include")
+  |	Pstr_attribute (attribute) -> raise (NotImplemented "Pstr_attribute")
+  |	Pstr_extension (extension, attributes) -> raise (NotImplemented "Pstr_extension")
+
+and dom_struct_items = function
+  | [] -> Xml.createNilDom()
+  | x :: xs -> dom_struct_item x
+
+and dom_list : 'a. (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a list -> unit
+    = fun ppr f xs -> raise (NotImplemented "dom_list")
+
+and dom_list' : 'a. (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a list -> unit
+    = fun ppr f xs -> match xs with
+        | [] -> raise (NotImplemented "dom_list' empty")
+        | x::r -> raise (NotImplemented "dom_list' cons")
+
+and dom_int_block n = dom_block "int_typed" [dom_field "INT" (string_of_int n)]
+
+and dom_id_block id = dom_block "variables_get_typed" [dom_var_field "VAR" false id]
+
+and dom_bool_block isTrue =
+    let upper_value = if isTrue then "TRUE" else "FALSE" in
+    dom_block "logic_boolean_typed" [dom_field "BOOL" upper_value]
+(*
+and dom_let_block binding expr2 = match binding with
+  | BiNil(loc) -> raise (NotImplemented "BiNil")
+    (* bi, bi *) (* let a = 42, dom_c f = function 43 *)
+  | BiAnd(loc, binding1, binding2) -> raise (NotImplemented "BiAnd")
+    (* p = e *) (* let patt = expr *)
+  | BiEq(loc, patt, expr1) ->
+      begin
+        match patt with
+        | PaId(loc, IdLid(_, name)) ->
+          let field = dom_var_field "VAR" true name in
+          let dom = dom_block "let_typed" [field] in
+          let domExp1 = dom_expr expr1 in
+          let domExp2 = dom_expr expr2 in
+          let dom = append_value dom "EXP1" domExp1 in
+          let dom = append_value dom "EXP2" domExp2 in
+          dom
+        | _ -> raise (NotImplemented "BiEq")
+      end
+    (* $s$ *)
+  | BiAnt(loc, name) -> raise (NotImplemented "BiAnt")
+*)
+and dom_app_block expr1 expr2 =
+    let domExp1 = dom_expr expr1 in
+    let domExp2 = dom_expr expr2 in
+    let dom = dom_block "lambda_app_typed" [] in
+    let dom = append_value dom "FUN" domExp1 in
+    let dom = append_value dom "ARG" domExp2 in
+    dom
+
+and dom_block typeName children =
+    Xml.createDom "block" [("type", typeName)] children
+
+and dom_block_value name child =
+    Xml.createDom "value" [("name", name)] [child]
+
+and append_value xml name child =
+    Xml.appendChild xml (dom_block_value name child)
+
+and dom_field name text =
+    Xml.createDom "field" [("name", name)] [Xml.createTextDom text]
+
+and dom_var_field name isValue text =
+    let field = dom_field name text in
+    Xml.setAttribute field ("isvalue", if isValue then "true" else "false")
---
+++ xml.ml
@@ -0,0 +1,85 @@
+type xml =
+  | Element of (string * (string * string) list * xml list)
+  | Text of string
+  | Nil
+
+let createDom tagName attr children =
+  Element (tagName, attr, children)
+
+let createTextDom text =
+  Text text
+
+let createNilDom () =
+  Nil
+
+let appendChild container child = match container with
+  | Element (tagName, attrs, childNodes) ->
+    Element (tagName, attrs, childNodes @ [child])
+  | Text _
+  | Nil -> failwith "Can't append xml node to text/nil node."
+
+let appendText container text =
+  appendChild container (createTextDom text)
+
+let setAttribute xml attr = match xml with
+  | Element (tagName, attrs, childNodes) ->
+    Element (tagName, attrs @ [attr], childNodes)
+  | Text _
+  | Nil -> failwith "Can't set attribute to text/nil node."
+
+let escape_map =
+  [('&', "&amp;"); ('<', "&lt;"); ('>', "&gt;"); ('\'', "&apos;");
+   ('\"', "&quot;")]
+let escape_targets = List.map fst escape_map
+
+let escape_string str =
+  let len = String.length str in
+  let sub f l = String.sub str f (l - f + 1) in
+  let rec step i prev =
+    if i < 0 || len < i then
+      assert false
+    else if i = len then
+      sub prev (i - 1)
+    else
+      let c = String.get str i in
+      if List.mem c escape_targets then
+        let substr = if i = 0 then "" else sub prev (i - 1) in
+        let s = List.assoc c escape_map in
+        substr ^ s ^ (step (i + 1) (i + 1))
+      else
+        step (i + 1) prev
+  in
+  step 0 0
+
+let printAttr (prop, value) =
+  prop ^ "=\"" ^ (escape_string value) ^ "\""
+
+let rec printAttrs attrs = match attrs with
+  | [] -> ""
+  | x :: xs ->
+    (printAttr x) ^ (if xs = [] then "" else " " ^ (printAttrs xs))
+
+let rec repeat_space n = if n <= 0 then "" else " " ^ (repeat_space (n - 1))
+
+let rec print' depth xml =
+  let ident = repeat_space depth in
+  match xml with
+  | Element (tagName, attrs, childNodes) ->
+    let attrStr = printAttrs attrs in
+    let childStr =
+      childNodes
+      |> List.map (print' (depth + 1))
+      |> List.fold_left (fun str x -> str ^ "\n" ^ x) ""
+    in
+    if attrs = [] then
+      ident ^ "<" ^ tagName ^ ">" ^ childStr ^ "\n" ^
+      ident ^ "</" ^ tagName ^ ">"
+    else
+      ident ^ "<" ^ tagName ^ " " ^ attrStr ^ ">" ^ childStr ^ "\n" ^
+      ident ^ "</" ^ tagName ^ ">"
+  | Text (text) ->
+    ident ^ (escape_string text)
+  | Nil ->
+    ""
+
+let print = print' 0
